9b4f7c5d2c4ecb6a66c0cef10dc81533e15349b5
francopozo <59775646+francopozo@users.noreply.github.com>
2024-02-22 16:25:58 -0400

    test

---
+++ fade over time.js
@@ -0,0 +1,10 @@
+
+transition = 20;       
+if (marker.numKeys<2){
+tSecs = transition / ( 1 / thisComp.frameDuration); 
+linear(time, inPoint, inPoint + tSecs, 0, 100)
+ - linear(time, outPoint - tSecs, outPoint, 0, 100)
+}else{
+linear(time, inPoint, marker.key(1).time, 0, 100) 
+- linear(time, marker.key(2).time, outPoint, 0, 100)
+}
